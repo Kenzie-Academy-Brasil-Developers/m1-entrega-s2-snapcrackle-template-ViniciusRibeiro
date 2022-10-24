e90cd170856cf9138459ce29e553e1d9ebbb7b67
vinicius <viniciusribeiro.dev@hotmail.com>
2022-10-24 16:03:11 -0300

    Entrega - Javascript: SnapCrackle

---
+++ js.js
@@ -0,0 +1,15 @@
+function snapCrackle(maxValue) {
+  let string = "";
+  for (let i = 1; i <= maxValue; i++) {
+    if (i % 5 == 0 && i % 2 == 1) {
+      string += "SnapCrackle, ";
+    } else if (i % 2 == 1) {
+      string += "Snap, ";
+    } else if (i % 5 == 0) {
+      string += "Crackle, ";
+    } else {
+      string += `${i}, `;
+    }
+  }
+  return string;
+}
